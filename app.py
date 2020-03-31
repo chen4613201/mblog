0ab7b2a170e56baad701e2e6ddae772990a8360a
chen4613201 <493408294@qq.com>
2020-04-01 01:01:39 +0800

    modify

--- app.py
+++ app.py
@@ -4,9 +4,27 @@ app = Flask(__name__)
 app.config['DEBUG']=True
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template("index.html")
 
+
+@app.route('/Sentiment')
+def Sentiment():
+    return render_template("Sentiment.html")
+
+
+@app.route('/Summary')
+def Summary():
+    return render_template("Summary.html")
+
+@app.route('/AboutUs')
+def AboutUs():
+    return render_template("AboutUs.html")
+
+
+@app.route('/message')
+def message():
+    return render_template("message.html")
 
 if __name__ == '__main__':
     app.run()
---
+++ static/js/navbar.js
@@ -0,0 +1,35 @@
+function click_sentiment_link(){
+    $('#web_left_content').load("/Sentiment",null,function(response,status){
+        if(status!="success"){
+            console.log(status);
+            $('#web_right_content').html(status);
+        }
+    });
+}
+
+function click_summary_link(){
+    $('#web_left_content').load("/Summary",null,function(response,status){
+        if(status!="success"){
+            console.log(status);
+            $('#web_right_content').html(status);
+        }
+    });
+}
+
+function click_aboutus_link(){
+    $('#web_left_content').load("/AboutUs",null,function(response,status){
+        if(status!="success"){
+            console.log(status);
+            $('#web_right_content').html(status);
+        }
+    });
+}
+
+function click_message_link(){
+    $('#web_left_content').load("/message",null,function(response,status){
+        if(status!="success"){
+            console.log(status);
+            $('#web_right_content').html(status);
+        }
+    });
+}
