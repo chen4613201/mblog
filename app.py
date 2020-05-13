49fa49788e003895d4a1619092aea8d30437930b
chen4613201 <493408294@qq.com>
2020-05-13 11:26:43 +0800

    modify

--- app.py
+++ app.py
@@ -2,12 +2,18 @@
 from flask import Flask,render_template,request,url_for
 from flask_bootstrap import Bootstrap
 from dbext import db
+from flask_script import Manager
+from flask_migrate import Migrate,MigrateCommand
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 
 
 bootstrap = Bootstrap(app)
 db.init_app(app)
+manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command("db", MigrateCommand)
+
 from dbs import C_T_User,C_T_Article
 from UserModule import UserModule
 from ArticleModule import ArticleModule
@@ -23,4 +29,4 @@ def index():
 
 if __name__ == '__main__':
     print(app.url_map)
-    app.run(host='127.0.0.1',port=5000,debug=True)
+    app.run()
